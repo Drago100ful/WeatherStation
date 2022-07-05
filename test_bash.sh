e14167339809277e59defba6e9138becb644673c
Chris Kerpa <chris.kerpa@mennen.online>
2022-07-05 19:35:02 +0200

    Bugfix 12

--- test_bash.sh
+++ test_bash.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+    echo "$0 is not running as root. Try using sudo."
+    exit 2
+fi
+
 sudo mkdir /usr/bin/WeatherStation/
 sudo cp ./readout.py /usr/bin/WeatherStation/
 
-sudo echo "
-[Unit]
+sudo echo "[Unit]
 Description=Starts Sensor Readout
 After=multi-user.target
 
@@ -15,3 +20,6 @@ Restart=on-abort
 [Install]
 WantedBy=multi-user.target
 " > /lib/systemd/system/sensor_readout.service
+
+sudo systemctl enable sensor_readout.service
+sudo systemctl start sensor_readout
