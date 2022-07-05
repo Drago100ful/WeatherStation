1272c30598f92b87939256f9ad963d873a0d7be0
Chris Kerpa <chris.kerpa@mennen.online>
2022-07-05 17:10:02 +0200

    Created Mobile UI

--- weather-app/rest/api.php
+++ weather-app/rest/api.php
@@ -1,12 +1,13 @@
 <?php
+include "db.php";
+
 header("Content-Type:application/json");
 header("Access-Control-Allow-Origin: *");
-
 
 function getData($data)
 {
     if (mysqli_num_rows($data) > 1) {
-        $collection = array();
+        $collection = [];
         $index = 0;
         while ($row = mysqli_fetch_array($data, MYSQLI_ASSOC)) {
             $collection[$index] = $row;
@@ -15,10 +16,22 @@ function getData($data)
 
         return $collection;
     } elseif (mysqli_num_rows($data) > 0) {
-        return mysqli_fetch_array($data);
-    }
+        $data = mysqli_fetch_array($data);
 
-    return NULL;
+        return [
+            "temp" => $data["temp"],
+            "pressure" => $data["pressure"],
+            "humidity" => $data["humidity"],
+            "altitude" => $data["altitude"],
+        ];
+    }
+    echo json_encode(["error" => "No results"]);
+    die();
+}
+
+function getCurrentDate()
+{
+    return date("Y-m-d G:i:s");
 }
 
 function respond($data)
@@ -30,17 +43,26 @@ function respond($data)
     }
 }
 
-function getCurrentDate()
+function getDataBetween($difference)
 {
-    return date('Y-m-d G:i:s');
+    global $database_connection;
+    return mysqli_query(
+        $database_connection,
+        "SELECT * FROM data_log WHERE log_date BETWEEN '" .
+        timeSub($difference) .
+        "' AND '" .
+        getCurrentDate() .
+        "'"
+    );
 }
 
 function timeSub($difference)
 {
-    return date('Y-m-d G:i:s', strtotime(getCurrentDate()) - $difference);
+    return date("Y-m-d G:i:s", strtotime(getCurrentDate()) - $difference);
 }
 
-function average($data) {
+function average($data): array
+{
     $size = count($data);
     $temp = 0;
     $pressure = 0;
@@ -55,63 +77,67 @@ function average($data) {
     }
 
     return [
-        round($temp /= $size, 1),
-        round($pressure /= $size, 1),
-        round($humidity /= $size, 1),
-        round($altitude /= $size, 1)
+        "temp" => round($temp /= $size, 1),
+        "pressure" => round($pressure /= $size, 1),
+        "humidity" => round($humidity /= $size, 1),
+        "altitude" => round($altitude /= $size, 1),
     ];
 }
 
-
-if (isset($_GET['getTemp'])) {
-
-    if (isset($_GET['timespan'])) {
-        global $database_connection;
-        $timespan = htmlspecialchars($_GET['timespan']);
-        include("db.php");
+if (isset($_GET["getTemp"])) {
+    global $database_connection;
+    if (isset($_GET["timespan"])) {
+        $timespan = htmlspecialchars($_GET["timespan"]);
 
         switch ($timespan) {
             case "current":
-                $data = mysqli_query($database_connection, "SELECT * FROM data_log ORDER BY log_date DESC LIMIT 1");
+                $data = mysqli_query(
+                    $database_connection,
+                    "SELECT * FROM data_log ORDER BY log_date DESC LIMIT 1"
+                );
                 respond(getData($data));
                 break;
+            case "5s":
+                $data = getDataBetween(5);
+                respond(average(getData($data)));
+                break;
             case "30s":
-                $data = mysqli_query($database_connection, "SELECT * FROM data_log WHERE log_date BETWEEN '" . timeSub(30) . "' AND '" . getCurrentDate() . "'");
-                respond(getData($data));
+                $data = getDataBetween(30);
+                respond(average(getData($data)));
 
                 break;
             case "60s":
-                $data = mysqli_query($database_connection, "SELECT * FROM data_log WHERE log_date BETWEEN '" . timeSub(60) . "' AND '" . getCurrentDate() . "'");
-                respond(getData($data));
+                $data = getDataBetween(60);
+                respond(average($data));
                 break;
             case "5min":
-                $data = mysqli_query($database_connection, "SELECT * FROM data_log WHERE log_date BETWEEN '" . timeSub(300) . "' AND '" . getCurrentDate() . "'");
-                respond(getData($data));
+                $data = $data = getDataBetween(300);
+                respond(average($data));
                 break;
             case "30min":
-                $data = mysqli_query($database_connection, "SELECT * FROM data_log WHERE log_date BETWEEN '" . timeSub(1800) . "' AND '" . getCurrentDate() . "'");
-                respond(getData($data));
+                $data = $data = getDataBetween(1800);
+                respond(average($data));
                 break;
             case "1h":
-                $data = mysqli_query($database_connection, "SELECT * FROM data_log WHERE log_date BETWEEN '" . timeSub(3600) . "' AND '" . getCurrentDate() . "'");
+                $data = $data = getDataBetween(3600);
                 $data = getData($data);
 
                 respond(average($data));
                 break;
             case "12h":
-                $data = mysqli_query($database_connection, "SELECT * FROM data_log WHERE log_date BETWEEN '" . timeSub(43200) . "' AND '" . getCurrentDate() . "'");
+                $data = $data = getDataBetween(43200);
                 $data = getData($data);
 
                 respond(average($data));
                 break;
             case "1d":
-                $data = mysqli_query($database_connection, "SELECT * FROM data_log WHERE log_date BETWEEN '" . timeSub(86400) . "' AND '" . getCurrentDate() . "'");
+                $data = $data = getDataBetween(86400);
                 $data = getData($data);
 
                 respond(average($data));
                 break;
             case "7d":
-                $data = mysqli_query($database_connection, "SELECT * FROM data_log WHERE log_date BETWEEN '" . timeSub(604800) . "' AND '" . getCurrentDate() . "'");
+                $data = $data = getDataBetween(604800);
                 $data = getData($data);
 
                 respond(average($data));
@@ -120,6 +146,4 @@ if (isset($_GET['getTemp'])) {
         mysqli_close($database_connection);
         die();
     }
-
 }
-
--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -1,34 +1,121 @@
 <template>
-  <div>
+  <div :class="darkMode ? 'dark' : ''">
+    <div
+      class="flex h-screen w-screen flex-col transition duration-300 dark:text-slate-50"
+    >
+      <div
+        class="flex flex-row items-center bg-slate-50 p-4 transition duration-300 dark:border-b dark:border-b-slate-700 dark:bg-slate-900"
+      >
+        <button @click="toggleTheme">
+          <svg
+            class="h-8 w-8 text-slate-500 hover:text-slate-900 transition dark:text-slate-50 duration-300"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
 
+      <div class="h-full transition duration-300 dark:text-slate-50">
+        <div
+          class="flex h-5/6 h-full flex-col justify-center bg-slate-100 drop-shadow transition duration-300 dark:bg-slate-900"
+        >
+          <div class="w-full">
+            <h1
+              class="text-center text-7xl font-semibold text-slate-900 opacity-95 transition duration-300 dark:text-slate-50"
+            >
+              {{ parsedData.temp }}°
+            </h1>
+          </div>
+        </div>
+        <div
+          class="h-1/6 w-full bg-slate-200 p-4 transition duration-300 dark:bg-slate-800"
+        >
+          <div class="flex flex-col">
+            <div
+              class="flex flex-row justify-between text-slate-900 transition duration-300 dark:font-normal dark:text-slate-50"
+            >
+              <p class="opacity-95">{{ parsedData.humidity }}%</p>
+              <p class="opacity-95">
+                {{ parseInt(parsedData.altitude) + 54 }}m
+              </p>
+              <p class="opacity-95">{{ parsedData.pressure / 1000 }} bar</p>
+            </div>
+          </div>
+
+          <select
+            v-model="timespan"
+            class="mt-8 w-full appearance-none rounded-lg bg-slate-300 p-1 px-2 text-center text-slate-900 opacity-95 transition duration-300 focus:outline-0 dark:bg-slate-700 dark:text-slate-50"
+          >
+            <option value="current">Aktuell</option>
+            <option value="5s">Letzten 5 Sekunden</option>
+            <option value="30s">Letzten 30 Sekunden</option>
+            <option value="1min">Letzte Minute</option>
+            <option value="30min">Letzten 30 Minuten</option>
+            <option value="1h">Letzte 1 Stunde</option>
+            <option value="12h">Letzte 12 Stunden</option>
+            <option value="1d">Letzter Tag</option>
+            <option value="7d">Letzten 7 Tage</option>
+          </select>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
-
   data() {
     return {
       parsedData: {},
-      timespan: "5"
-    }
+      timespan: "current",
+      darkMode: false,
+      options: [
+        "Aktuell",
+        "Letzten 5 Sekunden",
+        "Letzten 30 Sekunden",
+        "Letzte Minute",
+        "Letzte 30 Minuten",
+        "Letzte Stunde",
+        "Letzten 12 Stunden",
+        "Letzter Tag",
+        "Letzten 7 Tage",
+      ],
+    };
   },
-
+  watch: {
+    timespan(newTimespan, oldTimespan) {
+      if (newTimespan !== oldTimespan) {
+        this.getData();
+      }
+    },
+  },
   created() {
     this.getData();
     this.interval = setInterval(() => this.getData(), 5000);
   },
 
   methods: {
+    toggleTheme() {
+      this.darkMode = !this.darkMode;
+    },
 
     getData() {
-      fetch("https://127.0.0.1/weathertest/api.php?getTemp&timespan=" + this.timespan)
-          .then(respone => respone.json())
-          .then(data => (this.parsedData = data))
-    }
-  }
-
-
-}
+      fetch(
+        "https://127.0.0.1/weathertest/api.php?getTemp&timespan=" +
+          this.timespan
+      )
+        .then((respone) => respone.json())
+        .then((data) => (this.parsedData = data));
+    },
+  },
+};
 </script>
