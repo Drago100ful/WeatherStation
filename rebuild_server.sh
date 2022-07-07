e4b06a81cee523fe6d29b6cd2a00c89881e16bfc
Chris Kerpa <chris.kerpa@mennen.online>
2022-07-07 13:29:10 +0200

    Addded rebuild_server.sh

---
+++ rebuild_server.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ "$(id -u)" -ne "$ROOTUID" ] ; then
+    echo "This script must be executed with root privileges."
+    exit 1
+fi
+
+echo "IP-Adresse des Hosts: "
+
+read hostip
+
+prfx="{\"host-address\": \""
+sufx="\"}"
+concat=$prfx$hostip$sufx
+
+clear
+
+echo "OK! \n"
+sudo echo "$concat" > ./weather-app/src/assets/config.json
+cd ./weather-app/
+npm install
+npm run build
+
+
+sudo rm -rf /var/www/html
+sudo mkdir /var/www/html
+sudo cp -a ./rest/. /var/www/html
+sudo cp -a ./dist/. /var/www/html
